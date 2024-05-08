ac75998f082ab9134b9d09a5990e581c10d8ed4f
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-05-08 10:57:23 +0200

    exercici 2 i 3

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const jokeElement = document.getElementById("joke");
 const jokeBtn = document.getElementById("next-joke");
 const options = {
@@ -6,14 +15,31 @@ const options = {
         Accept: "application/json",
     },
 };
-const fetchJoke = () => {
-    fetch("https://icanhazdadjoke.com/", options)
-        .then((res) => res.json())
-        .then((data) => {
+let jokeReport = [];
+const fetchJoke = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const res = yield fetch("https://icanhazdadjoke.com/", options);
+        const data = yield res.json();
         jokeElement.innerHTML = data.joke;
-    });
-};
+    }
+    catch (error) {
+        jokeElement.innerHTML = "Oops! Couldn't find any joke...";
+    }
+});
 fetchJoke();
 jokeBtn.addEventListener("click", () => {
     fetchJoke();
 });
+const addScore = (score) => {
+    const joke = jokeElement.innerHTML;
+    const date = new Date().toISOString();
+    const jokeIndex = jokeReport.findIndex((report) => report.joke === joke);
+    if (jokeIndex !== -1) {
+        jokeReport[jokeIndex].score = score;
+        jokeReport[jokeIndex].date = date;
+    }
+    else {
+        jokeReport.push({ joke, score, date });
+    }
+    console.log(jokeReport);
+};
--- index.ts
+++ index.ts
@@ -6,12 +6,20 @@ const options = {
   },
 };
 
-const fetchJoke = () => {
-  fetch("https://icanhazdadjoke.com/", options)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeElement.innerHTML = data.joke;
-    });
+let jokeReport: {
+  joke: string;
+  score: 1 | 2 | 3;
+  date: string;
+}[] = [];
+
+const fetchJoke = async () => {
+  try {
+    const res = await fetch("https://icanhazdadjoke.com/", options);
+    const data = await res.json();
+    jokeElement.innerHTML = data.joke;
+  } catch (error) {
+    jokeElement.innerHTML = "Oops! Couldn't find any joke...";
+  }
 };
 
 fetchJoke();
@@ -19,3 +27,17 @@ fetchJoke();
 jokeBtn.addEventListener("click", () => {
   fetchJoke();
 });
+
+const addScore = (score: 1 | 2 | 3) => {
+  const joke = jokeElement.innerHTML;
+  const date = new Date().toISOString();
+  const jokeIndex = jokeReport.findIndex((report) => report.joke === joke);
+
+  if (jokeIndex !== -1) {
+    jokeReport[jokeIndex].score = score;
+    jokeReport[jokeIndex].date = date;
+  } else {
+    jokeReport.push({ joke, score, date });
+  }
+  console.log(jokeReport);
+};
